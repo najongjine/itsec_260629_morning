21b0db7dc9c41e89f082a3379219435f4adc1ad4
najongjine <najongjin3@hotmail.com>
2026-06-30 11:47:57 +0900

    .

---
+++ dummy.py
@@ -0,0 +1,20 @@
+"""
+주석 이라고 합니다.
+뭔 뜻이냐면, 이건 컴퓨터가 컴파일 안해요
+"""
+"""
+객체, class, relation, entity
+    속성, 변수, 필드
+"""
+class User:
+    name="pepe"
+    age=20
+    
+    """
+    method, 함수, 컴포넌트
+    """
+    def showdata(self):
+        print(self.name)
+
+
+
