7e439daa043a574ce2b0f1e8a8afa6d4f929427d
najongjine <najongjin3@hotmail.com>
2026-07-03 11:35:56 +0900

    .

--- p20260703.py
+++ p20260703.py
@@ -8,3 +8,26 @@ score가 60이상이면 result가 "D"
 """
 score=77
 result=""
+"""
+if score >= 90 :
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+elif score >= 60:
+    print("D")
+else :
+    print("F")
+"""
+
+if score >= 90 :
+    print("A")
+if 89>= score >= 80:
+    print("B")
+if 79 >= score >= 70:
+    print("C")
+if 69 >= score >= 60:
+    print("D")
+if score < 60:
+    print("F")
