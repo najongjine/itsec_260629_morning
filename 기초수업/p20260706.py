6a536f6b56535e7c03e6b6f82db6162c807f0276
najongjine <najongjin3@hotmail.com>
2026-07-06 11:36:04 +0900

    .

--- 기초수업/p20260706.py
+++ 기초수업/p20260706.py
@@ -1,20 +1,33 @@
 for i in range(0,10):
-    print(f"i:{i}")
+    #print(f"i:{i}")
+    pass
 
 for i in [0,1,2,3,4,5,6,7,8,9]:
-    print(f"i:{i}")
+    #print(f"i:{i}")
+    pass
+    
 
 print("프로그램 끝")
 
 
 for a in range(1,10):
-    print(f"2 * {a}  = {2*a}")
+    #print(f"2 * {a}  = {2*a}")
+    pass
 
 for a in range(1,10):
-    print(f"3 * {a}  = {3*a}")
+    #print(f"3 * {a}  = {3*a}")
+    pass
 
 
 for a in range(1,5):
     for b in range(0,3):
-        print(f"a:{a},b:{b}")
+        #print(f"a:{a},b:{b}")
+        pass
+
+
+for a in range(2,10):
+    for b in range(1,10):
+        print(f"{a}*{b}={a*b}")
+        pass
+
 
