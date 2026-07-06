ec98f3c38b6cd1db87c2727a1da440fc3c5d059c
najongjine <najongjin3@hotmail.com>
2026-07-06 11:26:47 +0900

    .

--- 기초수업/p20260706.py
+++ 기초수업/p20260706.py
@@ -10,3 +10,11 @@ print("프로그램 끝")
 for a in range(1,10):
     print(f"2 * {a}  = {2*a}")
 
+for a in range(1,10):
+    print(f"3 * {a}  = {3*a}")
+
+
+for a in range(1,5):
+    for b in range(0,3):
+        print(f"a:{a},b:{b}")
+
