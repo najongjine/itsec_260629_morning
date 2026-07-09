54f003143c051ffa4138404237e39d93ae2965cb
najongjine <najongjin3@hotmail.com>
2026-07-09 11:17:50 +0900

    .

---
+++ 기초수업/p20260709.py
@@ -0,0 +1,14 @@
+class Student:
+    """ ___init___ 이걸 생성자라고 해요
+    클래스를 객체로 실체화할때 꼭 실행되요
+    class의 함수 매개변수 에는 self라는게 꼭 들어가야해요
+    """
+    def __init__(self):
+        self.name="페페"
+        self.score=0
+        pass
+    pass
+
+s=Student()
+print(s.name)
+print(s.score)
