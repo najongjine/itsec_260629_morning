77783ad0da19044d7b446e31482d1a2bc53f60d4
najongjine <najongjin3@hotmail.com>
2026-07-09 11:22:37 +0900

    .

--- 기초수업/p20260709.py
+++ 기초수업/p20260709.py
@@ -3,9 +3,9 @@ class Student:
     클래스를 객체로 실체화할때 꼭 실행되요
     class의 함수 매개변수 에는 self라는게 꼭 들어가야해요
     """
-    def __init__(self):
-        self.name="페페"
-        self.score=0
+    def __init__(self,name,score):
+        self.name=name
+        self.score=score
         pass
     pass
 
