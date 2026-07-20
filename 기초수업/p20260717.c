080414dfa609d05665a18efc940f7314b9a5afeb
najongjine <najongjin3@hotmail.com>
2026-07-20 11:12:18 +0900

    .

--- 기초수업/p20260717.c
+++ 기초수업/p20260717.c
@@ -22,6 +22,17 @@ int myrand() {
     mynum = random_num;
     return mynum;
 }
+void sort(int * ar,int len) {
+    for (int a = 0; a < len - 1; a++) {
+        for (int b = 0; b < len - 1; b++) {
+            if (ar[b] > ar[b + 1]) {
+                int t = ar[b];
+                ar[b] = ar[b + 1];
+                ar[b + 1] = t;
+            }
+        }
+    }
+}
 int main() {
     int arr[6] = { 0,0,0,0,0,0 };
     // 배열 길이 구하기
@@ -41,9 +52,11 @@ int main() {
         }
         arr[i++] = num1;
     }
+    sort(arr,length);
     for (int i = 0; i < length; i++) {
         printf("%d  ", arr[i]);
     }
+    
 
     return 0;
 }
