5f578fc465a3f4bbfb7292a97c5c322db1ff5c66
najongjine <najongjin3@hotmail.com>
2026-07-21 09:44:36 +0900

    .

--- 기초수업/dummy.py
+++ 기초수업/dummy.py
@@ -1,6 +1,16 @@
-def myf():
-    pass
-
-arr="hello nice to meet you"
-
-print(arr)
+class LocationDict:
+    locations = {
+        "NYC": "New York",
+        "LON": "London",
+        "PAR": "Paris",
+        "TKY": "Tokyo"
+    }
+ 
+tmpdict = LocationDict()
+str01 = ""
+for key, location in tmpdict.locations.items():
+    keyk = key[-1]
+    locationk = location[0]
+    str01 += keyk + locationk
+ 
+print(str01, end="")
