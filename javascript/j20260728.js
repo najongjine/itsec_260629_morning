b4f3d2893b3d3d06c103f39b9c6d8cff49b38a44
najongjine <najongjin3@hotmail.com>
2026-07-28 11:11:30 +0900

    .

---
+++ javascript/j20260728.js
@@ -0,0 +1,56 @@
+if(1){
+  let a=1;
+}else{
+
+}
+if(1){
+  let a=1;
+}
+else if(1){
+
+}else{
+
+}
+
+let a=[1,2,3];
+
+for (let index = 0; index < a.length; index++) {
+  const element = a[index];
+}
+
+a.forEach((e, index) => {
+  a[index] = e * e;
+});
+console.log(a)
+
+for (const e of a) {
+  
+}
+
+
+function dummy(a,func1){
+  let r=func1();
+  return {a:22};
+}
+const dummy2=(a)=>{
+  return {a:22};
+}
+
+/*
+파이썬: int, double, boolean, dic, set, tuple, list, string, class......
+자바스크립트는 : number, boolean, JSObject, string, class
+프로그래밍엔 2가지 스타일이 있습니다
+ 객체 1등급 - 자바
+ 함수 1등급 - 자바스크립트
+
+자바스크립트의 장점은 물과같아서 좋아요.
+모든 데이터를 다 받아서 처리할수 있어요.
+단점은 물과같아서 안좋아요.
+ */
+
+
+const user = {
+  name: "홍길동"
+};
+
+console.log(user.age); // undefined
