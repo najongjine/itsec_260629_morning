ee46118375fc7d4d922137a670e7ed3db6600270
najongjine <najongjin3@hotmail.com>
2026-08-06 11:37:37 +0900

    .

--- itsec202607_memo/src/BoardList.tsx
+++ itsec202607_memo/src/BoardList.tsx
@@ -12,7 +12,6 @@ function BoardList() {
   let [content,set_content]=useState("");
   let [boardList,set_boardList]=useState<BoardType[]>([]);
 
-
   return (
     <div>
       <div> 게시판 리스트에요 </div>
@@ -24,6 +23,14 @@ function BoardList() {
           set_title2(title);
           console.log(title);
         } } />
+      </div>
+      <div>
+        <label>내용:</label>
+      </div>
+      <div>
+        <textarea placeholder='내용을 입력하세요'
+        cols={50} rows={20}>
+        </textarea>
       </div>
       <div>내가 입력한거: {title2}</div>
     </div>
