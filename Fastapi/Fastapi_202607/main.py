b0b0cc4e81c77a6a310a1e8a342e3c6339538918
najongjine <najongjin3@hotmail.com>
2026-08-12 11:46:19 +0900

    ./

---
+++ Fastapi/Fastapi_202607/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+
+app=FastAPI()
+
+@app.get("/")
+def home():
+    return {"success":True,"msg":"ㅎㅇ"}
+
+
+if __name__=="__main__":
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
+    )
