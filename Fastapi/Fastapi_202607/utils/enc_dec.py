7ac6e2414101529b9d202a38b6b2f456a2c61164
najongjine <najongjin3@hotmail.com>
2026-08-20 11:21:03 +0900

    .

--- Fastapi/Fastapi_202607/utils/enc_dec.py
+++ Fastapi/Fastapi_202607/utils/enc_dec.py
@@ -13,7 +13,7 @@ password_hash=PasswordHash.recommended()
 def hash_password(password:str) -> str:
     """
     비밀번호를 단방향 해시합니다.
-    
+
     parameter:
         password (str): 사용자가 입력한 평문 비밀번호.
 
@@ -21,3 +21,19 @@ def hash_password(password:str) -> str:
         str: Argon2 라는 사람이 만든방식의 암호화된 비밀번호
     """
     return password_hash.hash(password)
+
+def verify_password(plain_password:str
+                    ,hashed_password:str) -> bool:
+    """
+    입력한 비밀번호가 암호화된거랑 일치하는지 검증.
+
+    parameter:
+        plain_password (str): 사용자가 입력한 평문 비밀번호.
+
+        hashed_password (str): 암호화된 비밀번호.
+
+    returns:
+        bool.
+    """
+    return password_hash.verify(plain_password
+                                ,hashed_password)
