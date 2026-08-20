a15a38191a390c4f5b6a6aef6971322175882cf0
najongjine <najongjin3@hotmail.com>
2026-08-20 11:16:55 +0900

    .

--- Fastapi/Fastapi_202607/utils/enc_dec.py
+++ Fastapi/Fastapi_202607/utils/enc_dec.py
@@ -3,3 +3,21 @@ from cryptography.fernet import Fernet
 import hashlib
 import hmac
 import base64
+
+"""
+단반향 암호화 (비밀번호용)
+"""
+password_hash=PasswordHash.recommended()
+
+
+def hash_password(password:str) -> str:
+    """
+    비밀번호를 단방향 해시합니다.
+    
+    parameter:
+        password (str): 사용자가 입력한 평문 비밀번호.
+
+    returns:
+        str: Argon2 라는 사람이 만든방식의 암호화된 비밀번호
+    """
+    return password_hash.hash(password)
