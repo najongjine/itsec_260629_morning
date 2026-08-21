bce2d4cc52c6c569d6443c629337226e80e610f5
najongjine <najongjin3@hotmail.com>
2026-08-21 11:15:46 +0900

    .

--- Fastapi/Fastapi_202607/utils/jwtutil.py
+++ Fastapi/Fastapi_202607/utils/jwtutil.py
@@ -24,3 +24,14 @@ def create_access_token(
         ,algorithm=ALGORITHM
     )
     return token
+
+def decode_access_token(token:str):
+    try:
+        payload= jwt.decode(
+            token
+            ,SECRET_KEY
+            ,algorithms=[ALGORITHM]
+        )
+        return payload
+    except InvalidTokenError:
+        return None
