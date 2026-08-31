668de6473c947d5ed4a9e40e09767598c3d9cd11
najongjine <najongjin3@hotmail.com>
2026-08-31 11:05:38 +0900

    .

--- itsec202607_memo/src/component/Header.tsx
+++ itsec202607_memo/src/component/Header.tsx
@@ -1,17 +1,32 @@
 import { useState } from 'react';
 import { useEffect } from "react";
 import { Link } from 'react-router';
+import { useAuth } from '../auth';
 
 
 function Header() {
-
+  const { isLoggedIn, user, token, logout,login } = useAuth()
 
   return (
     <div>
       <div> 
         <Link to="/"> 홈 </Link>
         <Link to="/boardlist"> 게시판 </Link>
-        <Link to="/login"> 로그인 </Link>
+        {!isLoggedIn && (
+          <div>
+            <Link to="/login"> 로그인 </Link>
+          </div>
+        )}
+        {isLoggedIn && (
+          <div>
+            <div>{user?.username}</div>
+            <div>
+              <button onClick={(e)=>{
+                logout();
+              }}>로그아웃</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
