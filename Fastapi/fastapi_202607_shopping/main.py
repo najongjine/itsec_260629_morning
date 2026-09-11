3220a1cba0a6c9fd876463aeb663f29e90c2a04c
najongjine <najongjin3@hotmail.com>
2026-09-11 11:38:40 +0900

    .

---
+++ Fastapi/fastapi_202607_shopping/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Form
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+
+#from routers.router_example import router as example_router
+
+# 서버 뿅 하고 완성 됨
+app=FastAPI()
+app.add_middleware(
+    CORSMiddleware
+    ,allow_origins=["*"]
+    ,allow_credentials=True
+    ,allow_methods=["*"]
+    ,allow_headers=["*"]
+)
+
+#app.include_router(example_router,tags=["예제 API"])
+
+# api endpoint, router, controller
+@app.get("/")
+def healthcheck():
+    return {"success":True,"msg":"서버 건강함"}
+
+if __name__=="__main__":
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000, # 3000~65000 중 맘대로 정함
+        reload=True # ctrl + s 했을때 서버 자동으로 재시작
+    )
+
---
+++ Fastapi/fastapi_202607_shopping/routers/user_router.py
@@ -0,0 +1,20 @@
+from fastapi import APIRouter, Form, Header
+
+router=APIRouter()
+
+@router.post("/register")
+def register(
+    username=Form("")
+    ,password=Form("")
+    ,email=Form("")
+    ,gender=Form("m")
+):
+    result={"success":True
+            ,"data":None
+            ,"msg":""}
+    try:
+        pass
+    except Exception as e:
+        result["success"]=False
+        result["msg"]=str(e)
+    return result
